a3293314d063be9e956366d66ebe496f5c0b590e
Ahmed Mohsen <ahmedthemohsen@gmail.com>
2025-06-25 17:21:26 +0300

    created app styles

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -5,7 +5,6 @@ class AppColors {
   static const Color terracotta = Color(0xffcc7861);
   static const Color brownRosie = Color(0xffdcbeb6);
   static const Color beige = Color(0xfffaf0e6);
-  static const Color orangeDark = Color(0xffcc7861);
   static const Color white = Colors.white;
   static const Color black = Color(0xff363130);
 }
---
+++ lib/core/constants/app_styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:ideas_and_concepts/core/constants/app_colors.dart';
+
+class AppStyles {
+  static const salmon24 = TextStyle(color: AppColors.salmon, fontSize: 24);
+  static const black28 = TextStyle(color: AppColors.black, fontSize: 28);
+  static const black20 = TextStyle(color: AppColors.black, fontSize: 20);
+  static const black22 = TextStyle(color: AppColors.black, fontSize: 22);
+  static const black18 = TextStyle(color: AppColors.black, fontSize: 18);
+  static const white24 = TextStyle(color: AppColors.white, fontSize: 24);
+  static const blackItalic18 = TextStyle(
+    color: AppColors.black,
+    fontSize: 18,
+    fontStyle: FontStyle.italic,
+    fontWeight: FontWeight.w100,
+  );
+}
