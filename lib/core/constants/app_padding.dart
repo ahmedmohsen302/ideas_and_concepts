bd4e57932f5be2eb465b28033beb2f276169a602
Ahmed Mohsen <ahmedthemohsen@gmail.com>
2025-06-25 17:24:51 +0300

    created app padding

---
+++ lib/core/constants/app_padding.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppPadding {
+  static const hPadding16 = Padding(
+    padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+  );
+  static const hPadding20 = Padding(
+    padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+  );
+  static const vPadding10 = Padding(
+    padding: EdgeInsetsGeometry.symmetric(vertical: 10),
+  );
+  static const vPadding16 = Padding(
+    padding: EdgeInsetsGeometry.symmetric(vertical: 16),
+  );
+}
