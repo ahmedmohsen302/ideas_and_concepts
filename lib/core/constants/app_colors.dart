b176fed85eb3b5bebb6916b7a15e119969fb5425
Ahmed Mohsen <ahmedthemohsen@gmail.com>
2025-06-25 16:56:58 +0300

    created my app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color salmon = Color(0xffF4B5A4);
+  static const Color terracotta = Color(0xffcc7861);
+  static const Color brownRosie = Color(0xffdcbeb6);
+  static const Color beige = Color(0xfffaf0e6);
+  static const Color orangeDark = Color(0xffcc7861);
+  static const Color white = Colors.white;
+  static const Color black = Color(0xff363130);
+}
